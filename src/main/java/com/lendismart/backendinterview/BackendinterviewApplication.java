eece820fe778c7510629a609ee7bb6a504ef7a68
Peter <peter.catalin@lendismart.com>
2021-02-04 11:43:10 +0100

    Add Spring initializer generated files

---
+++ src/main/java/com/lendismart/backendinterview/BackendinterviewApplication.java
@@ -0,0 +1,13 @@
+package com.lendismart.backendinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendinterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lendismart/backendinterview/BackendinterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lendismart.backendinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendinterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
